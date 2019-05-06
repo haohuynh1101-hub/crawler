592c8da47d279e726da8bbb0604e5f1819da5a31
Hoàng Công Minh <congminhh17@gmail.com>
2019-05-06 15:11:19 +0700

    'login'

---
+++ src/app/routes/login/index.js
@@ -0,0 +1,24 @@
+var router = require('express').Router();
+var passport = require("passport");
+
+
+//dang nhap
+router.get('/', async  (req, res, next) => {
+    return res.render('login');
+});
+
+router.post("/", passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: false
+}), (req, res) => {
+    if (req.body.remember) {
+        req.session.cookie.maxAge = new Date(
+            Date.now() + 30 * 24 * 60 * 60 * 1000
+        ); // Cookie expires after 30 days
+    } else {
+        req.session.cookie.expires = false; // Cookie expires at end of session
+    }
+    res.redirect("/");
+}
+);
+module.exports = router
