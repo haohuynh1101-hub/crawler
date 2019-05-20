d4cf02a976f5f07773b93b6bf2009ff82874f747
Huy Cuong <17520306@gm.uit.edu.vn>
2019-05-20 17:52:21 +0700

    fix bug deploy

--- src/services/changeUserAgent.js
+++ src/services/changeUserAgent.js
@@ -1,7 +1,13 @@
 //var randomUseragent = require('random-useragent');
-var {randomAgent}=require('./randomAgent');
-const changeUserAgent=async()=>{
-    let result=await randomAgent();
-    return result;
+var { randomAgent } = require('./randomAgent');
+const changeUserAgent = async () => {
+    try {
+        let result = await randomAgent();
+        await page.setUserAgent(result);
+        return result;
+    } catch (error) {
+        console.log("TCL: changeUserAgent -> error", error)
+    }
+
 }
-module.exports=changeUserAgent;
+module.exports = changeUserAgent;
