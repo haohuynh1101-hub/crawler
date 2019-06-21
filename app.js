1462d5073558a0f2ce1a73a78bf51e59ee0e1af0
Michael John <duc.nguyenminh@2359media.com>
2019-06-21 10:11:57 +0700

    A :

--- app.js
+++ app.js
@@ -5,102 +5,65 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var passport = require("passport");
 var session = require('express-session');
-var cluster = require('cluster');
-var os = require('os');
-var { PORT } = require('./src/config');
-
 var config = require("config");
 
 require('dotenv').config();
 require('model/connect');
 require('model/schema');
 
+var app = express();
 
-if (cluster.isMaster) {
-  // we create a HTTP server, but we do not use listen
-  // that way, we have a socket.io server that doesn't accept connections
-  var server = require('http').createServer();
-  var io = require('socket.io').listen(server);
-  var redis = require('socket.io-redis');
+app.set("topSecretKey", config.SECRET);
+// view engine setup
+app.set('views', path.join(__dirname, 'src', 'app', 'views'));
+app.set('view engine', 'ejs');
 
-  io.adapter(redis({ host: 'localhost', port: PORT }));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-  setInterval(function() {
-    // all workers will receive this in Redis, and emit
-    io.emit('data', 'payload');
-  }, 1000);
+app.use(cookieParser("marketing-secret-via-signedCookie"));
+app.use(express.static(path.join(__dirname, 'src', 'app', 'public'), { maxAge: '30 days', dotfiles: 'allow' }));
+app.set('Cache-Control', 'max-age=3000');
 
-  for (var i = 0; i < os.cpus().length; i++) {
-    cluster.fork();
-  }
+app.use(
+  session(
+    config.SESSION
+  )
+);
 
-  cluster.on('exit', function(worker, code, signal) {
-    console.log('worker ' + worker.process.pid + ' died');
-  });
+
+//PassportJS middleware
+app.use(passport.initialize());
+app.use(passport.session()); //persistent login sessions
+
+require('config/passport')(passport);
+
+if(process.env.IS_DEV != 'DEV' || process.env.IS_DEV == 'undefined'){
+  app.disable('/setup');
+}
+else {
+  app.use('/setup', require('app/routes/setup'));
 }
 
-if (cluster.isWorker) {
-  var http = require('http');
-  var app = express();
-  var server = http.createServer(app);
-  var io = require('socket.io').listen(server);
-  var redis = require('socket.io-redis');
+app.use('/', require('app/routes'));
 
-  io.adapter(redis({ host: 'localhost', port: PORT }));
-  io.on('connection', function(socket) {
-    socket.emit('data', 'connected to worker: ' + cluster.worker.id);
-  });
+  // catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  res.render('error')
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  console.log(err);
   
-  app.set("topSecretKey", config.SECRET);
-  // view engine setup
-  app.set('views', path.join(__dirname, 'src', 'app', 'views'));
-  app.set('view engine', 'ejs');
-  
-  app.use(logger('dev'));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  
-  app.use(cookieParser("marketing-secret-via-signedCookie"));
-  app.use(express.static(path.join(__dirname, 'src', 'app', 'public'), { maxAge: '30 days', dotfiles: 'allow' }));
-  app.set('Cache-Control', 'max-age=3000');
-  
-  app.use(
-    session(
-      config.SESSION
-    )
-  );
-  
-  
-  //PassportJS middleware
-  app.use(passport.initialize());
-  app.use(passport.session()); //persistent login sessions
-  
-  require('config/passport')(passport);
-  
-  if(process.env.IS_DEV != 'DEV' || process.env.IS_DEV == 'undefined'){
-    app.disable('/setup');
-  }
-  else {
-    app.use('/setup', require('app/routes/setup'));
-  }
-  
-  app.use('/', require('app/routes'));
-  
-    // catch 404 and forward to error handler
-  app.use(function(req, res, next) {
-    res.render('error')
-  });
-  
-  // error handler
-  app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
-    // render the error page
-    res.status(err.status || 500);
-    console.log(err);
-    
-    res.render('error');
-  });
-}
+  res.render('error');
+});
+
+module.exports = app;
