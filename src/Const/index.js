34b7edc26063f0fcc05537bcb95f8ecadbc98bdb
Huy Cuong <17520306@gm.uit.edu.vn>
2019-06-21 14:53:08 +0700

    a

--- src/Const/index.js
+++ src/Const/index.js
@@ -2,7 +2,7 @@ var fs=require('fs');
 module.exports = {
     options: {
         headless: true, 
-        args: ['--no-sandbox', '--disable-setuid-sandbox','--proxy-server=37.48.118.90:13082']
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
     },
     //listProxy: fs.readFileSync('src/app/public/proxy.txt').toString().split("\n"),
     timeDelay:10000,
