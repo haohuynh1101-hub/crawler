d208fba34885a29701260db6a217e4e30ee05183
Huy Cuong <huycuongdn1999@gmail.com>
2020-03-25 09:18:23 +0700

    change db

--- src/model/connect.js
+++ src/model/connect.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect("mongodb://admin:123@103.142.139.46:27017/marketing_seo", { useNewUrlParser: true });
+mongoose.connect("mongodb://admin:123@103.75.180.209:27017/marketing_seo", { useNewUrlParser: true });
